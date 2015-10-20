dacf93f84b5654ddf913cb393c48feb96612bf73
Liam Marshall <archimedespi3141@gmail.com>
2015-10-19 19:53:12 -0500

    Set up all the motors in sampler.ino

--- firmware/sampler.ino
+++ firmware/sampler.ino
@@ -5,10 +5,13 @@ DcMotor xMotor(X_AXIS_PWM, X_AXIS_FWD, X_AXIS_REV),
   yMotor(Y_AXIS_PWM, Y_AXIS_FWD, Y_AXIS_REV),
   zMotor(Z_AXIS_PWM, Z_AXIS_FWD, Z_AXIS_REV);
 
+
 void setup() {
   Serial.begin(BAUDRATE);
   xMotor.begin();
-  xMotor.setPower(1);
+  yMotor.begin();
+  zMotor.begin();
 }
 
-void loop() {}
+void loop() {
+}
