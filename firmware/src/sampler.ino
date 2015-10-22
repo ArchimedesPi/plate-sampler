ca04db2843e46e273a94ee37c7100580f009a1b4
Liam Marshall <archimedespi3141@gmail.com>
2015-10-22 14:14:10 -0500

    Split motor definitions into multiple lines

--- firmware/src/sampler.ino
+++ firmware/src/sampler.ino
@@ -3,10 +3,9 @@
 #include "config.h"
 #include <DcMotor.h>
 
-DcMotor xMotor(X_AXIS_PWM, X_AXIS_FWD, X_AXIS_REV),
-  yMotor(Y_AXIS_PWM, Y_AXIS_FWD, Y_AXIS_REV),
-  zMotor(Z_AXIS_PWM, Z_AXIS_FWD, Z_AXIS_REV);
-
+DcMotor xMotor(X_AXIS_PWM, X_AXIS_FWD, X_AXIS_REV);
+DcMotor yMotor(Y_AXIS_PWM, Y_AXIS_FWD, Y_AXIS_REV);
+DcMotor zMotor(Z_AXIS_PWM, Z_AXIS_FWD, Z_AXIS_REV);
 
 void setup() {
   Serial.begin(BAUDRATE);
